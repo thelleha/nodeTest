244ebee244e9c45aebefbffaeeb65ab97c478c43
thelleha <thelleha@gmail.com>
2023-11-28 18:54:06 +0100

    add app
    app.js: implemented app
    
    package.json: add start-script + module

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-import chalk from "chalk";
+import chalk from 'chalk';
+
+const log = console.log;
+
+let joke = "Hva rimer på bilen? - Vinduene ...";
+let words = joke.split(" ");
+
+joke = "";
+for (let n in words) { 
+	let red = 63 + 32*random(0, 6);
+	let green = 63 + 32*random(0, 6);
+	let blue = 63 + 32*random(0, 6);
+	joke = joke + chalk.rgb(red, green, blue).bold(words[n]) + " ";
+}
+
+log("\n\n\n\n\n");
+log(joke);
+log("\n\n\n\n\n");
+ 
+
+
+
+
+function random(min, max) {
+	return Math.floor(Math.random() * (max-min)) + min;
+}
